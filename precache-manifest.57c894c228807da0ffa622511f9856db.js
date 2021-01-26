9171fbdfefb46a9b37adb0e144775f89ac4956f1
Sean <miniod2018@gmail.com>
2021-01-26 18:37:43 -0500

    first commit

---
+++ precache-manifest.57c894c228807da0ffa622511f9856db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a81ce5bad3dc181be62febc0d822e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49643b45fdfd600d311",
+    "url": "/static/css/main.5e32e588.chunk.css"
+  },
+  {
+    "revision": "1dfb76ecbc90be3f5baa",
+    "url": "/static/js/2.6195de68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6195de68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49643b45fdfd600d311",
+    "url": "/static/js/main.28fe3eb7.chunk.js"
+  },
+  {
+    "revision": "e1bb56cbc0b80f0c1852",
+    "url": "/static/js/runtime-main.446996b0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
